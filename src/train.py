5dabe1c53d31588356b11707bf07613a134d359e
Nitin Awasthi <rflinkbudget@gmail.com>
2025-08-01 17:32:30 +0530

    Removed deprecated utils.py and updated train.py to use my_utils.py

--- src/train.py
+++ src/train.py
@@ -2,15 +2,14 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.utils import load_data, evaluate_model
+from src import my_utils as utils
 
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import joblib
-#from src.utils import load_data, evaluate_model  # ✅ imported utils
 
 # Load data using utils
-X, y = load_data()
+X, y = utils.load_data()
 
 # Split into train-test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -21,14 +20,10 @@ model.fit(X_train, y_train)
 
 # Predict and evaluate
 y_pred = model.predict(X_test)
-mse, r2 = evaluate_model(y_test, y_pred)
+r2, mse = utils.compute_scores(y_test, y_pred)
 
 print(f"✅ R² Score: {r2:.4f}")
 print(f"✅ MSE Loss: {mse:.4f}")
 
 # Save trained model
 joblib.dump(model, "model.pth")
-
-
-
-
